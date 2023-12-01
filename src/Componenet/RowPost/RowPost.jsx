9cc16d95aa7b6866534dc5ffd3dafce6fdcf1152
hadhi hassan m <diluhadhi01@gmail.com>
2023-12-01 10:22:52 -0800

    add youtube videos urls and some css style addedd

--- src/Componenet/RowPost/RowPost.jsx
+++ src/Componenet/RowPost/RowPost.jsx
@@ -1,31 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import "./RowPost.css"
 import axios from '../../constants/axios'
-import {API_KEY,imageUrl} from '../../constants/constants'
+import { API_KEY, imageUrl } from '../../constants/constants'
+import Youtube from "react-youtube"
 const RowPost = (props) => {
     const [movies, setMovies] = useState([])
+    const [url, setUrlId] = useState("")
     useEffect(() => {
         axios
-        .get(props.url)
-        .then((responce)=>{
-            console.log(responce.data);
-            setMovies(responce.data.results)
-        })
-        .catch((error)=>{
-            // alert("that was a network error check you net")
-        })
+            .get(props.url)
+            .then((responce) => {
+                console.log(responce.data);
+                setMovies(responce.data.results)
+            })
+            .catch((error) => {
+                // alert("that was a network error check you net")
+            })
     }, [])
+
+    const opts = {
+        height: "389",
+        width: '100%',
+        playerVars: {
+            autoplay: 1,
+        },
+    }
+    const handleMovieVideo = (id) => {
+        console.log(id);
+        axios
+          .get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
+          .then((response) => {
+            console.log(response.data);
+            if(response.data.results.length !== 0){
+                setUrlId(response.data.results[0])
+            }
+          })
+          .catch((error) => {
+            console.error('Error fetching movie videos:', error);
+          });
+      };
+      
     return (
 
         <div className='row'>
             <h2>{props.title}</h2>
             <div className='posters'>
                 {
-                    movies.map((obj)=>{
-                         return <img alt="poster" className={props.isSmall ? 'small_poster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`}/>
+                    movies.map((obj) => {
+                        return <img onClick={() => handleMovieVideo(obj.id)} alt="poster" className={props.isSmall ? 'small_poster' : 'poster'} src={`${imageUrl + obj.backdrop_path}`} />
                     })
                 }
             </div>
+           { url && <Youtube opts={opts} videoId={url.key}/>}
         </div>
     )
 }
